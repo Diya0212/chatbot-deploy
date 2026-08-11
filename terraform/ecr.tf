82dc53f4758b7508e3bd0ac13f54ab8644ae9524
Diya0212 <diyabansal02@gmail.com>
2026-08-11 10:36:19 +0530

    infra: add ECR lifecycle policy to cap image storage at 10 images

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -10,3 +10,24 @@ resource "aws_ecr_repository" "chatbot" {
     Project = "chatbot"
   }
 }
+
+resource "aws_ecr_lifecycle_policy" "chatbot" {
+  repository = aws_ecr_repository.chatbot.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep only the last 10 images"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 10
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
